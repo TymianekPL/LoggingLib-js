011ba9c907617d2926a49c9397c326c2413d5bcb
tymianekpl <wilku.tymon@gmail.com>
2022-06-18 09:09:49 +0200

    💡 Add comments to typings file

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,7 @@
 declare module "@tymianekpl/LoggingLib" {
+     /**
+      * Options for LoggingLib
+      */
      type LogLibOptions = {
           /**
           * The category of the log. If null, the category is not printed.
@@ -30,29 +33,89 @@ declare module "@tymianekpl/LoggingLib" {
      };
 
 
+     /**
+      * The LoggingLib class.
+      */
      class LoggingLib {
+          /**
+           * Creates a new instance of the LoggingLib.
+           * @param options The options for the logging library.
+           */
           constructor(options?: LogLibOptions);
+
+          /**
+           * Logs a message.
+           * @param message The message to log.
+           */
           public info(message: string): void;
+          /**
+           * Logs a message.
+           * @param message The message to log.
+           * @param category The category of the log. If null, the category is not printed.
+           */
           public info(message: string, category: string): void;
 
+          /**
+           * Logs a warning.
+           * @param message The message to log.
+           */
           public warn(message: string): void;
+          /**
+           * Logs a warning.
+           * @param message The message to log.
+           * @param category The category of the log. If null, the category is not printed
+           */
           public warn(message: string, category: string): void;
 
+          /**
+           * Logs an error.
+           * @param message The message to log.
+           */
           public error(message: string): void;
+          /**
+           * Logs an error.
+           * @param message The message to log.
+           * @param category The category of the log. If null, the category is not printed
+           */
           public error(message: string, category: string): void;
 
+          /**
+           * Logs a debug message.
+           * @param message The message to log.
+           */
           public debug(message: string): void;
+          /**
+           * Logs a debug message.
+           * @param message The message to log.
+           * @param category The category of the log. If null, the category is not printed
+           */
           public debug(message: string, category: string): void;
 
-          public error(message: string): void;
-          public error(message: string, category: string): void;
-
+          /**
+           * Logs a success message.
+           * @param message The message to log.
+           */
           public success(message: string): void;
+          /**
+           * Logs a success message.
+           * @param message The message to log.
+           * @param category The category of the log. If null, the category is not printed
+           */
           public success(message: string, category: string): void;
 
+          /**
+           * Logs a notice.
+           * @param message The message to log.
+           */
           public notice(message: string): void;
+          /**
+           * Logs a notice.
+           * @param message The message to log.
+           * @param category The category of the log. If null, the category is not printed
+           */
           public notice(message: string, category: string): void;
      }
 
+     // export LoggingLib as default
      export = LoggingLib;
 }
